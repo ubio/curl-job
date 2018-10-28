c3b27ce4ff511193b2041d1e0dd7c7318a8baf4b
Mark S. Everitt <mark.s.everitt@gmail.com>
2018-10-28 23:23:17 +0000

    Corrects missing colon in URL.

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ UPPER_NAME=$(echo $KUBERNETES_SERVICE_NAME | tr 'a-z\-' 'A-Z_')
 eval "IP=\$${UPPER_NAME}_SERVICE_HOST"
 eval "PORT=\$${UPPER_NAME}_SERVICE_PORT"
 
-URL="$KUBERNETES_SERVICE_PROTOCOL$IP$PORT$PATHNAME"
+URL="$KUBERNETES_SERVICE_PROTOCOL$IP:$PORT$PATHNAME"
 
 status=$(curl -k -s -o /dev/null -w "%{http_code}" -X POST -d '' -u $USER: $URL)
 
